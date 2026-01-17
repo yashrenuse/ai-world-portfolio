ef96821d36b50f1227fa6eefdd6835c6da15f848
Yash Renuse <yashrenuse21@gmail.com>
2026-01-17 17:16:22 +0530

    First version of AI world portfolio

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Text } from "@react-three/drei";
+
+export default function Home() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [0, 2, 5] }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[5, 5, 5]} />
+
+        <Text fontSize={0.6} position={[0, 1, 0]}>
+          Welcome to Yash’s AI World
+        </Text>
+
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}
